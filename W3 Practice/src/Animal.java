417317e1fd31513fc3ca36ef0a929afb7cd9714c
rrruk <rrruk@naver.com>
2019-03-21 20:39:07 +0900

    0321-7 commit animal finished

--- W3 Practice/src/Animal.java
+++ W3 Practice/src/Animal.java
@@ -1,4 +1,27 @@
 
-public class Animal {
-	
+public abstract class Animal {
+
+    //instance variable
+    String food;
+    int hunger;
+    String boundaries;
+    String lacation;
+
+    //method
+    public void makeNoise() {
+        System.out.println("!!!");
+    }
+
+    public void roam() {
+        System.out.println("grrr");
+    }
+    
+    public void eat() {
+    	System.out.println("yum yum");
+    }
+
+    public void sleep() {
+        System.out.println("zzz");
+    }
+
 }
